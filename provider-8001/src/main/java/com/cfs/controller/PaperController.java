5dbf46a2355624247f1dcc5a124fa16526f4cb93
ASUS <2897218640@qq.com>
2022-03-20 10:09:26 +0800

    deleteCourse

--- provider-8001/src/main/java/com/cfs/controller/PaperController.java
+++ provider-8001/src/main/java/com/cfs/controller/PaperController.java
@@ -1,6 +1,7 @@
 package com.cfs.controller;
 
 import com.cfs.entities.CommonResult;
+import com.cfs.entities.PaperQuestion;
 import com.cfs.service.PaperService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,9 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @Slf4j
@@ -36,27 +35,55 @@ public class PaperController {
         return s;
     }
 
+//    @RequestMapping("/addPaper")
+//    public CommonResult<String> addPaper(@RequestBody HashMap<String, Object> map){
+//
+//
+//        List<Map> questionList = (List<Map>) map.get("questionList");
+//
+////        List questionList = Collections.singletonList(map.get("questionList"));
+//
+//        System.out.println(questionList);
+//        Integer result = 1;
+//
+//        return result > 0 ? new CommonResult<>(100, "添加成功") : new CommonResult<>(200, "添加失败");
+//    }
+
     @RequestMapping("/addPaper")
-    public CommonResult<String> addPaper(@RequestBody HashMap<String, String> map){
-        String token = map.get("token");
+    public CommonResult<String> addPaper(@RequestBody HashMap<String, Object> map){
+        String token = (String)map.get("token");
         String checkup = checkup(token);
 
         if (checkup == null) {
             return new CommonResult<>(200, "用户未登录或登录状态失效", null);
         }
 
-        String paperName = map.get("paperName");
-        Integer maxMark = Integer.parseInt(map.get("maxMark"));
+        String paperName = (String)map.get("paperName");
+        Integer maxMark = Integer.parseInt(map.get("maxMark").toString());
         Integer creatorId = Integer.parseInt(checkup);
         Timestamp createTime = new Timestamp(System.currentTimeMillis());
-        Integer courseId = Integer.parseInt(map.get("courseId"));
-        Integer change = Integer.parseInt(map.get("change"));
+        Integer courseId = Integer.parseInt(map.get("courseId").toString());
+        Integer change = Integer.parseInt(map.get("change").toString());
 
-//       List<Map<String,String>> questionList = map.get("questionList");
+        List<Map> questionList = (List<Map>) map.get("questionList");
+
+
+        Integer result2 = paperService.addPaperQuestion(questionList);
+
+
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//            if (iterator.next().get("questionType").equals("choice")&&iterator.next().get("private").equals("0")){
+//
+//            }
+//        }
 
         Integer result = paperService.addPaper(paperName,maxMark,creatorId,courseId,createTime,change);
 
         return result > 0 ? new CommonResult<>(100, "添加成功") : new CommonResult<>(200, "添加失败");
+
+//        return new CommonResult<>(100, "添加成功");
+
     }
 
 
